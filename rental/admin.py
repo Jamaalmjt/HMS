db3bcbdb5d5569dcc576494cffcdd35f6c122561
Jamaalmjt <108972901+Jamaalmjt@users.noreply.github.com>
2023-01-01 11:55:07 +0200

    Update admin.py

--- rental/admin.py
+++ rental/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import *
 
 admin.site.register(Amenities)
-admin.site.register(Hotel)
-admin.site.register(HotelImages)
-admin.site.register(HotelBooking)
+admin.site.register(rental)
+admin.site.register(rentalImages)
+admin.site.register(Reservation)
