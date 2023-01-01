9be79d17df2a80f1133e3308a5bd6cfaeb6b0df1
Jamaalmjt <108972901+Jamaalmjt@users.noreply.github.com>
2023-01-01 11:52:56 +0200

    Update urls.py

--- rental/urls.py
+++ rental/urls.py
@@ -5,9 +5,9 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('check_booking/' , check_booking),
-    path('', home , name='home'),
-    path('hotel-detail/<uid>/' , hotel_detail , name="hotel_detail"),
+    path('check_Reservation/' , check_Reservation),
+    path('', rental , name='rental'),
+    path('rental-detail/<uid>/' , rental_detail , name="rental_detail"),
     path('login/', login_page , name='login_page'),
     path('register/', register_page , name='register_page'),
 
